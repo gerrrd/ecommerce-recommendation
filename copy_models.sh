a1a634a4374d5e456f564319133012719fa83f56
Gergely Farkas <gergely.farkas.u@gmail.com>
2026-02-10 12:50:20 +0100

    Addig the LLM-embedding-extended Association Rules model.

--- copy_models.sh
+++ copy_models.sh
@@ -1,5 +1,6 @@
 cp notebooks/*.pkl app/models/
-# we remove the LLM model as it won't be used for now in the API
+# we remove the LLM and AR_LLM models as they won't be used for now in the API
 rm app/models/llm_model.pkl
+rm app/models/ARLLM_model.pkl
 cp notebooks/cache/*.pkl app/models/
 cp notebooks/cache/*.pkl ui/
